439e59617f99531a9da26235cec69af964dbaa4e
oliviadp <olivia.osypovych@gmail.com>
2024-12-29 14:33:43 +0200

    fixed modal

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import { useState } from 'react';
 import { fetchImages } from './services/api';
 import Loader from './components/Loader/Loader';
 import toast from 'react-hot-toast';
-import ImageGallery from './components/ImageGallery/ImageGallery/ImageGallery';
+import ImageGallery from './components/ImageGallery/ImageGallery';
 import ErrorMessage from './components/ErrorMessage/ErrorMessage';
 import LoadMoreBtn from './components/LoadMoreBtn/LoadMoreBtn';
+import ImageModal from "./components//ImageModal/ImageModal";
 
 
 
@@ -16,6 +17,7 @@ function App() {
   const [isError, setIsError] = useState(false);
   const [page, setPage] = useState(1);
   const [query, setQuery] = useState("");
+  const [selectedImage, setSelectedImage] = useState (null);
   
 
   const getImagesData = async (newQuery, newPage = 1) => {
@@ -53,13 +55,26 @@ function App() {
     }
   };
 
+
+
+const handleCardClick = (image) => {
+    setSelectedImage(image);
+  }
+
+const closeModal = () => {
+  setSelectedImage(null);
+}
+
   return (
     <>
       <SearchBar onSubmit={handleChangeQuery} />
-      {images.length > 0 && <ImageGallery images={images} />}
+      {images.length > 0 && <ImageGallery images={images} handleCardClick={ handleCardClick} />}
       {images.length > 0 && !isLoading && <LoadMoreBtn handleChangePage={handleChangePage}/>}
       {isLoading && <Loader />}
       {isError && <ErrorMessage />}
+      {selectedImage && <ImageModal image={selectedImage}
+      isOpen ={!!selectedImage}
+    onRequestClose={closeModal}/>}
     </>
   );
 }
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { toast } from "react-hot-toast";
 import s from "./SearchBar.module.css";
 import { TbSearch } from "react-icons/tb";
